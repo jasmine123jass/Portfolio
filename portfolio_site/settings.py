3c33859d631829dbd12bde5649cf1091f4d64158
sruji <srujithajasmineb@gmail.com>
2026-03-23 09:46:36 +0530

    fixed email env variables

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -99,8 +99,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'baggamsrujithajasmine@gmail.com'
-EMAIL_HOST_PASSWORD = 'lkcz owfq bsqn tpht'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 CONTACT_RECIPIENT = 'baggamsrujithajasmine@gmail.com'
 
 CONTACT_EMAIL = 'baggamsrujithajasmine@gmail.com'
